5aa3abc45ef23378f8f52b0a4b87fa483751ce32
saiful10400 <saifulislam10400i5@gmail.com>
2026-06-26 16:39:25 +0600

    create:config file

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+import mongoose from "mongoose"
 import app from "./app"
-
+import dbUrl from "./config/index"
 const PORT = process.env.PORT || 4000
 
 async function main() {
+    
     try {
+        // await mongoose.connect(dbUrl.dbUrl as string,{dbName:"rokto-khuji"})
         app.listen(PORT, () => {
             console.log(`Server is running on http://localhost:${PORT}`)
         })
